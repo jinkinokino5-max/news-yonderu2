667779a70dff58dcec3c7cdc75ea9279386b5f15
jinkinokino5-max <jinkinokino5@gmail.com>
2026-05-04 16:59:11 +0900

    Update Supabase URL and key in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://dyvupejxkfktayylqcoh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5dnVwZWp4a2ZrdGF5eWxxY29oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjczNzYsImV4cCI6MjA5MzQ0MzM3Nn0.-wxRzRqmuVwk6_0IUpa2I96fZqmbBXJtwneUetBXlag';
 const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
