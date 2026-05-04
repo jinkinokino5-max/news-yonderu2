dcffcdd1e36c1bf30ef140ca9748ffa6a3bc267c
jinkinokino5-max <jinkinokino5@gmail.com>
2026-05-04 18:41:07 +0900

    Change GitHub PAT in config.js
    Updated GitHub Personal Access Token in config.js.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://dyvupejxkfktayylqcoh.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5dnVwZWp4a2ZrdGF5eWxxY29oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjczNzYsImV4cCI6MjA5MzQ0MzM3Nn0.-wxRzRqmuVwk6_0IUpa2I96fZqmbBXJtwneUetBXlag';
-const GH_PAT = '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';
+const GH_PAT = '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';
 
 const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
